d7bf1c18a9cdbad11e4babf54e8e8b1300019161
Andrew Horwitz <ahwitz@gmail.com>
2014-07-28 14:04:40 -0400

    New: changing pages in one document changes pages in the rest
    -Added "Part" class
    
    -Separated pagedidchange listener so it could be unsubscribed so it was not repeatedly called
    
    -All init functions are called after Ajax query for partbook json file
    
    -diva-wrappers are created programatically
    
    -python script now feeds one composition number per page (as it should)

--- parsecsv.py
+++ parsecsv.py
@@ -1,16 +1,23 @@
 import csv
 import os
 import json
+import string
 
 fileList = [f for f in os.listdir('csv') if (os.path.isfile('csv/' + f) and f.endswith('.csv'))]
 csvDicts = {}
 
 for curFile in fileList:
+	dictIndex = string.split(curFile, ".")[0]
 	csvFile = open('csv/' + curFile, 'rU')
 	csvDict = csv.DictReader(csvFile)
-	csvDicts[curFile] = []
+	csvDicts[dictIndex] = []
+	curPage = 1
 	for curLine in csvDict:
-		csvDicts[curFile].append(curLine['compositionKey'])
-
+		while len(csvDicts[dictIndex]) <= int(curLine['folio_end']):
+			csvDicts[dictIndex].append(curLine['compositionKey'])
+			curPage += 1
+			
 with open('partbooks.json', 'w') as outfile:
 	json.dump(csvDicts, outfile)
+
+print "Completed."
