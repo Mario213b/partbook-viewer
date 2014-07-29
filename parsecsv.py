b157aef3a8c8369c66ccd97a413783833fa375ad
Andrew Horwitz <ahwitz@gmail.com>
2014-07-29 13:49:56 -0400

    New: piece catalog arranged alphabetically
    -Restructured parsecsv to return pieces in alphabetical order
    
    -Matched actions of index.html to necessary restructuring of parsecs
    
    -Changed location of unsubscribe/resubscribe to VisiblePageDidChange
    
    -Added pageOffsets object to help flip to correct pages

--- parsecsv.py
+++ parsecsv.py
@@ -2,6 +2,7 @@ import csv
 import os
 import json
 import string
+import operator
 
 fileList = [f for f in os.listdir('csv') if (os.path.isfile('csv/' + f) and f.endswith('.csv'))]
 csvDicts = {}
@@ -11,17 +12,17 @@ for curFile in fileList:
 	dictIndex = string.split(curFile, ".")[0]
 	csvFile = open('csv/' + curFile, 'rU')
 	csvDict = csv.DictReader(csvFile)
-	csvDicts[dictIndex] = []
-	curPage = 1
+	csvDicts[dictIndex] = {}
+
 	for curLine in csvDict:
 		if curLine['compositionKey'] not in pieces:
-			#print curLine
 			pieces[curLine['compositionKey']] = curLine['text_incipit_standard Copy'] 
-		while len(csvDicts[dictIndex]) <= int(curLine['folio_end']):
-			csvDicts[dictIndex].append(curLine['compositionKey'])
-			curPage += 1
-			
-assembledDict = {'pieces': pieces, 'csvData': csvDicts}
+
+		csvDicts[dictIndex][curLine['compositionKey']] = range(int(curLine['folio_start']), int(curLine['folio_end']))
+
+sortedPieces = sorted(pieces.iteritems(), key=operator.itemgetter(1))
+
+assembledDict = {'pieces': sortedPieces, 'csvData': csvDicts}
 
 with open('partbooks.json', 'w') as outfile:
 	json.dump(assembledDict, outfile, ensure_ascii=False)
